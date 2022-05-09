7bc5904ea8153bd698800e449e3fcb5729da5e8a
TokioTakamiya <takamiya.tokio.1@gmail.com>
2022-05-09 14:30:35 +0900

    作成中

---
+++ python_udemy/lesson.py
@@ -0,0 +1,6 @@
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+url_ranking = "https://data.j-league.or.jp/SFTD07/search?selectedCompetitionFrames=2&beginCompetitionYear=1992&endCompetitionYear=&totalAppearanceCount=100"
